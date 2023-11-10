f114b27d64e0d3a9d4166a6a920cdb886cd5c30c
Jjongday <118797848+Jjongday@users.noreply.github.com>
2023-11-10 11:15:40 +0800

    Update form-supplies.php

--- FINALLL/form-supplies.php
+++ FINALLL/form-supplies.php
@@ -54,66 +54,75 @@
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Clinic: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="heading" value="<?php if(isset($_POST['heading'])){echo $_POST['heading'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="clinic" value="<?php if(isset($_POST['clinic'])){echo $_POST['clinic'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Campus: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="button_text" value="<?php if(isset($_POST['button_text'])){echo $_POST['button_text'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="campus" value="<?php if(isset($_POST['campus'])){echo $_POST['campus'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Medicines: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="button_url" value="<?php if(isset($_POST['button_url'])){echo $_POST['button_url'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="medicines" value="<?php if(isset($_POST['medicines'])){echo $_POST['medicines'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Quantity: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="button_url" value="<?php if(isset($_POST['button_url'])){echo $_POST['button_url'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="quantity" value="<?php if(isset($_POST['quantity'])){echo $_POST['quantity'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Date Received: </label>
                                 <div class="col-sm-9">
-                                    <input type="date" autocomplete="off" class="form-control" name="heading" value="<?php if(isset($_POST['heading'])){echo $_POST['heading'];} ?>">
+                                    <input type="date" autocomplete="off" class="form-control" name="date_received" value="<?php if(isset($_POST['date_received'])){echo $_POST['date_received'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Remarks: </label>
                                 <div class="col-sm-9">
-                                    <textarea class="form-control" name="content" style="height:140px;"><?php if(isset($_POST['content'])){echo $_POST['content'];} ?></textarea>
+                                    <textarea class="form-control" name="remarks" style="height:140px;"><?php if(isset($_POST['remarks'])){echo $_POST['remarks'];} ?></textarea>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Supplies: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="heading" value="<?php if(isset($_POST['heading'])){echo $_POST['heading'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="supplies" value="<?php if(isset($_POST['supplies'])){echo $_POST['supplies'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Quantity: </label>
                                 <div class="col-sm-9">
-                                    <input type="text" autocomplete="off" class="form-control" name="button_text" value="<?php if(isset($_POST['button_text'])){echo $_POST['button_text'];} ?>">
+                                    <input type="text" autocomplete="off" class="form-control" name="quantity" value="<?php if(isset($_POST['quantity'])){echo $_POST['quantity'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Date Received: </label>
                                 <div class="col-sm-9">
-                                    <input type="date" autocomplete="off" class="form-control" name="heading" value="<?php if(isset($_POST['heading'])){echo $_POST['heading'];} ?>">
+                                    <input type="date" autocomplete="off" class="form-control" name="date_received" value="<?php if(isset($_POST['date_received'])){echo $_POST['date_received'];} ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label">Remarks: </label>
                                 <div class="col-sm-9">
-                                    <textarea class="form-control" name="content" style="height:140px;"><?php if(isset($_POST['content'])){echo $_POST['content'];} ?></textarea>
+                                    <textarea class="form-control" name="remarks" style="height:140px;"><?php if(isset($_POST['remarks'])){echo $_POST['remarks'];} ?></textarea>
                                 </div>
                             </div>
-
-
-                            
+                            <div class="form-group">
+                                <label for="" class="col-sm-2 control-label">Prepared by: </label>
+                                <div class="col-sm-9">
+                                    <input type="text" autocomplete="off" class="prepared_by" name="button_text" value="<?php if(isset($_POST['prepared_by'])){echo $_POST['prepared_by'];} ?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="col-sm-2 control-label">Noted by: </label>
+                                <div class="col-sm-9">
+                                    <input type="text" autocomplete="off" class="form-control" name="noted_by" value="<?php if(isset($_POST['noted_by'])){echo $_POST['noted_by'];} ?>">
+                                </div>
+                            </div>
                             <div class="form-group">
                                 <label for="" class="col-sm-2 control-label"></label>
                                 <div class="col-sm-9">
